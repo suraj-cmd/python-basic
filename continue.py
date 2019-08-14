fe7e5e0b0c13aa678d5d74cf1568c05749320f78
suraj-cmd <surajpatil522@gmail.com>
2019-08-14 22:42:15 +0530

    continue

---
+++ continue.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+while True:
+    n = int(input("no:"))
+    if n < 0:
+        continue
+    elif n == 0:
+     break
+    print("sq is", n ** 2)
+print("bye")
+
---
+++ pm.py
@@ -0,0 +1,9 @@
+import sys
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n -1)
+def main(n):
+    res = fact(n)
+    print(res)
+
