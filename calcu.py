c513cf55627da6983ca0ea37acd90ec725b7b253
suraj-cmd <surajpatil522@gmail.com>
2019-08-28 21:39:07 +0530

    unittest example

---
+++ calcu.py
@@ -0,0 +1,15 @@
+import unittest
+import multi
+class ExampleCase(unittest.TestCase):
+    
+    def test_description(self):
+        result = multi.add(50,50)
+        self.assertEqual(result,10)
+    
+    def test_description(self):
+        result = multi.multi(5,2)
+        self.assertEqual(result,10)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
--- unit2.py
+++ unit2.py
@@ -7,10 +7,6 @@ class TestCalc(unittest.Testcase):
         result = calc.add(10,5)
         self.assertEqual(result,15)
 
-   # def test_multi(self):
-    #    result = multi(10,5)
-     #   self.assertEqual = (result,50)
-
 
 if __name__ == '__main__':
     unittest.main()
