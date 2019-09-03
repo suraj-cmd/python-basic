50771e67159a9a8782c3fd46b574b466438b12dc
suraj-cmd <surajpatil522@gmail.com>
2019-09-03 11:59:46 +0530

    updates done by...

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ class TestCalc(unittest.TestCase):
     def test_add(self):
         result = calc.add(10,5)
         self.assertEqual(result,15)
+    def test_sub(self):
+        result = calc.sub(100,50)
+        self.assertEqual(result,50)
 
 if __name__ == '__main__':
     unittest.main()
--- unit2.py
+++ unit2.py
@@ -6,7 +6,9 @@ class TestCalc(unittest.Testcase):
     def test_add(self):
         result = calc.add(10,5)
         self.assertEqual(result,15)
-
+   # def test_sub(self):
+    #    result = calc.sub(200,100)
+     #   self.assertEqual(result,100)
 
 if __name__ == '__main__':
     unittest.main()
