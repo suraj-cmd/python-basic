9f3deff8c7c195f1c4c8803cd8a1073baf67deb3
Suraj Patil <53961650+suraj-cmd@users.noreply.github.com>
2019-09-10 16:55:05 +0530

    Delete input.py

--- input.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-number = int(input("Enter an integer: "))
-if number < 100:
-    print("Your number is smaller than 100")
-else:
-    print("Your number is greater than 100")
