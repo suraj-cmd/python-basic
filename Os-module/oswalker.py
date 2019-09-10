32ae977b4a9594acb9e981bc70a080d9daf11682
Suraj Patil <53961650+suraj-cmd@users.noreply.github.com>
2019-09-10 16:56:49 +0530

    Create oswalker.py

---
+++ Os-module/oswalker.py
@@ -0,0 +1,12 @@
+import os
+
+path = '/home/suraj'
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.txt' in file:
+            files.append(os.path.join(r, file))
+
+for f in files:
+    print(f)
