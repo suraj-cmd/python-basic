18c5948ebe4be2465e4f24a0176a59f335f2df0d
Suraj Patil <53961650+suraj-cmd@users.noreply.github.com>
2019-09-10 16:51:43 +0530

    Delete demo4.py

--- demo4.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
-    def myfun(self):
-        print("hello my name is " + self.name)
-p1 = Person("john",36)
-p1.myfun()
