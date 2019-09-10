fdef2d7267daa646eabaa149cef89c37ae344fa5
Suraj Patil <53961650+suraj-cmd@users.noreply.github.com>
2019-09-10 17:00:50 +0530

    Delete unit5.py

--- unit5.py
+++
@@ -1,8 +0,0 @@
-import unittest
-class TestCase(unittest.TestCase):
-    def Exampletest(self):
-        a = 'none'
-        b = 'none'
-        self.assertEqual(a, b)
-if __name__ == '__main__':
-    unittest.main(verbose=2)
