1d70576f02e7a209263ed2d329187bcd680fcaea
Suraj Patil <53961650+suraj-cmd@users.noreply.github.com>
2019-09-12 15:24:59 +0530

    Create employee.py

---
+++ unittest-mocking/employee.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class employee:
+
+    def __init__(self, first, last, pay):
+
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    @property
+    def email(self):
+
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+
+        return '{}.{}'.format(self.first, self.last)
